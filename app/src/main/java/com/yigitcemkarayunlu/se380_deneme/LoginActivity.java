d117c3fc00bed9d41d45f5b3d8825c6217564e70
Cem <Cem>
2016-04-17 22:59:13 +0300

    vvv

--- app/src/main/java/com/yigitcemkarayunlu/se380_deneme/LoginActivity.java
+++ app/src/main/java/com/yigitcemkarayunlu/se380_deneme/LoginActivity.java
@@ -41,7 +41,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     /**
      * Id to identity READ_CONTACTS permission request.
-     */
+     */ //llllllllll
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
